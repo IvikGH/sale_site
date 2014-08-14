09a26909431544be7b99c060c45d7dc7f4d3716d
Ivik <andoidxx@gmail.com>
2014-08-14 12:24:06 +0300

    rails project created repo initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaleSite::Application.config.secret_key_base = '9f8c23a6b3790b318cd5c267a871a562aea45712c7ae208a738215fc424e6d8381caef4d73dd7a7f11baddd217d4a618aad252c1b71ca716722d72d0280b0a09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaleSite::Application.config.session_store :cookie_store, key: '_sale_site_session'
